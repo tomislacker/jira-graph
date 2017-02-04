661bed9ef9459116ef68e263dd631033389a7cc4
Ben Tomasik <tomislacker@users.noreply.github.com>
2017-02-04 11:42:50 -0600

    Add issue styling

--- jira_graph/util.py
+++ jira_graph/util.py
@@ -6,6 +6,15 @@ Contains utility classes/methods
 """
 
 
+ISSUE_STATUS_COLORS = {
+    'in progress': '#bcebff',
+    'new': '#bcffd0',
+    'closed': '#ffd0bc',
+    '_DEFAULT_': '#ffbceb',
+}
+"""Define fill colors for issues based on status"""
+
+
 def get_issue_blocks(issue, link_types=['Blocked'], raise_fail=True):
     """ Returns an tuple of blocking and blocked issues
     """
@@ -30,3 +39,35 @@ def get_issue_blocks(issue, link_types=['Blocked'], raise_fail=True):
                         "Could not determine linkage direction")
 
     return (blocks, blocked_by)
+
+
+def get_issue_color(issue, default_color='_DEFAULT_'):
+    """ Returns a hex code for an issue's color based on status
+    """
+    status = issue.fields.status.name.lower()
+    if status in ISSUE_STATUS_COLORS:
+        return ISSUE_STATUS_COLORS[status]
+
+    # No color for status defined, send default
+    return ISSUE_STATUS_COLORS[default_color]
+
+
+def get_issue_styling(issue, *args, **kwargs):
+    """ Returns a diict of additional keys for a node
+    """
+    styles = {}
+
+    # Add the label
+    styles.update({
+        'label': "\n".join([
+            issue.key,
+            '(' + issue.fields.status.name + ')',
+        ])
+    })
+    # Add background color
+    styles.update({
+        'style': 'filled',
+        'fillcolor': get_issue_color(issue, *args, **kwargs)
+    })
+
+    return styles
